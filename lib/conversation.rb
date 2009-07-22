184e162841525e808968d727fbda72539baaefec
Michael Bulat <mbulat@facesmedia.com>
2009-07-22 17:04:20 -0400

    adding field validations to conversation and message

--- lib/conversation.rb
+++ lib/conversation.rb
@@ -2,4 +2,6 @@ class Conversation < ActiveRecord::Base
   has_many :messages
   has_many :conversation_participants
   has_many :participants, :through => :conversation_participants
+
+  validates_presence_of :subject
 end
--- lib/message.rb
+++ lib/message.rb
@@ -2,4 +2,6 @@ class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :receiver, :polymorphic => true  
   belongs_to :sender, :polymorphic => true  
+  
+  validates_presence_of :body
 end
